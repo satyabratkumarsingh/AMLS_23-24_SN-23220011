f47254e4774a53f3abaa1265d6c65e431edfe726
Satyabrat Kumar <satya@Satyabrats-MacBook-Pro.local>
2023-12-28 13:05:15 +0000

    more code

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from B.PathMNIST_multi_classification import PathMNISTImageMultiClassification
-
+from A.svm_binary_classification import SVMBinaryClassification
 
 
 def main():
-    image_multi_class = PathMNISTImageMultiClassification()
-    image_multi_class.download_images()
+    svm_binary_classification = SVMBinaryClassification()
+    svm_binary_classification.train_model()
+
+    #image_multi_class = PathMNISTImageMultiClassification()
+    #image_multi_class.download_images()
 
 
 if __name__ == "__main__":
